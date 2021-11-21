6a13577d9bf634a44791b301333cda4969e1bb9b
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2021-11-21 17:22:04 +0000

    Added ability to display multiple pages in the demo.

--- Test/Screen/Demo.cs
+++ Test/Screen/Demo.cs
@@ -1,19 +1,23 @@
-﻿using Test.Pages;
+﻿using SadConsole.Input;
+using Test.Pages;
 
 namespace Test.Screen;
 
 internal class Demo : ScreenObject
 {
-    readonly Header _header = new();
+    readonly Header _header;
     readonly DisplayArea _displayArea = new();
 
     readonly Page[] _pages = new Page[]
     {
+        new LoadingImages(),
         new LoadingImages()
     };
 
     public Demo()
     {
+        _header = new(_pages[0], _pages.Length);
+
         Game.Instance.Screen = this;
         Game.Instance.DestroyDefaultStartingConsole();
 
@@ -21,6 +25,41 @@ internal class Demo : ScreenObject
         Children.Add(_displayArea);
 
         _displayArea.Add(_pages[0]);
-        _header.SetHeader(_pages[0]);
+
+        IsFocused = true;
+    }
+
+    public override bool ProcessKeyboard(Keyboard keyboard)
+    {
+        if (keyboard.HasKeysPressed)
+        {
+            if (keyboard.IsKeyPressed(Keys.Left))
+                return PrevPage();
+            else if (keyboard.IsKeyPressed(Keys.Right))
+                return NextPage();
+        }
+        return true;
+    }
+
+    bool NextPage() => ChangePage(_pages.Length - 1, 1, _pages[0]);
+
+    bool PrevPage() => ChangePage(0, -1, _pages.Last());
+
+    bool ChangePage(int testIndex, int step, Page overlappingPage)
+    {
+        // get the index of current page
+        int currentPageIndex = Array.IndexOf(_pages, _displayArea.Children[0]);
+        _displayArea.Children.Clear();
+
+        // pull the next page from array and display it
+        int nextIndex = currentPageIndex + step;
+        var newPage = currentPageIndex == testIndex ? overlappingPage : _pages[nextIndex];
+        _displayArea.Children.Add(newPage);
+
+        // change header title and summary to describe the page
+        _header.SetHeader(newPage, step == -1 ? Direction.Left : Direction.Right);
+
+        // handled
+        return true;
     }
 }
--- Test/Screen/Header.cs
+++ Test/Screen/Header.cs
@@ -3,16 +3,67 @@
 internal class Header : ScreenSurface
 {
     public const int Height = 2;
-    public Header() : base(Program.Width, Height)
+    readonly PageCounter _pageCounter;
+
+    public Header(Page startPage, int pageCount) : base(Program.Width, Height)
     {
         Surface.DefaultBackground = Color.DarkGray;
         Surface.DefaultForeground = Color.Yellow;
+        _pageCounter = new PageCounter(pageCount) { Parent = this };
+        _pageCounter.Position = (Surface.Width - _pageCounter.Surface.Width, 0);
+        SetHeader(startPage);
     }
 
-    public void SetHeader(Page page)
+    public void SetHeader(Page page, Direction d)
+    {
+        SetHeader(page);
+
+        if (d == Direction.Left)
+            _pageCounter.PrevPage();
+        else
+            _pageCounter.NextPage();
+    }
+
+    void SetHeader(Page page)
     {
         Surface.Clear();
         Surface.Print(1, 0, page.Title.ToUpper());
         Surface.Print(1, 1, page.Summary, Color.Gray);
+    }
+}
+
+internal class PageCounter : ScreenSurface
+{
+    const string Title = "Page:";
+    readonly int _pageCount;
+    int _currentPage = 1;
+
+    public PageCounter(int pageCount) : base(Title.Length + 2, Header.Height)
+    {
+        Surface.DefaultBackground = Color.DarkGray.GetDarker();
+        Surface.DefaultForeground = Color.Yellow;
+        Surface.Clear();
+        _pageCount = pageCount;
+        Surface.Print(0, 0, Title.Align(HorizontalAlignment.Center, Surface.Width));
+        DisplayCounter();
+    }
+
+    public void NextPage()
+    {
+        _currentPage++;
+        if (_currentPage > _pageCount) _currentPage = 1;
+        DisplayCounter();
+    }
+
+    public void PrevPage()
+    {
+        _currentPage--;
+        if (_currentPage < 1) _currentPage = _pageCount;
+        DisplayCounter();
+    }
+
+    void DisplayCounter()
+    {
+        Surface.Print(0, 1, $"{_currentPage: 00}/{_pageCount:00}", Color.White);
     }
 }
