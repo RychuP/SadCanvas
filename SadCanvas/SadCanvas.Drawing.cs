e7ff3bf586707ca14d2be3d4ce28a6fc24ed28d7
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2021-11-25 21:48:59 +0000

    Removed the last methods using SadConsole.Color.

--- SadCanvas/SadCanvas.Drawing.cs
+++ SadCanvas/SadCanvas.Drawing.cs
@@ -7,12 +7,6 @@ public partial class Canvas : ScreenObject, IDisposable
     readonly string OutOfRangeMsg = "Pixel position is out of range";
 
     /// <summary>
-    /// Fills the area with a <see cref="Color"/>.
-    /// </summary>
-    /// <param name="color">Color to fill the <see cref="Canvas"/> with.</param>
-    public void Fill(Color color) => Fill(color.ToMonoColor());
-
-    /// <summary>
     /// Fills the area with a <see cref="MonoColor"/>.
     /// </summary>
     /// <param name="color"><see cref="MonoColor"/> to fill the <see cref="Canvas"/> with.</param>
@@ -31,14 +25,6 @@ public partial class Canvas : ScreenObject, IDisposable
     }
 
     /// <summary>
-    /// Changes the <see cref="Color"/> of a pixel at the given position.
-    /// </summary>
-    /// <param name="position">Position of the pixel.</param>
-    /// <param name="color"><see cref="Color"/> of the pixel.</param>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when the position of the pixel is invalid.</exception>
-    public void SetPixel(Point position, Color color) => SetPixel(position, color.ToMonoColor());
-
-    /// <summary>
     /// Changes the <see cref="MonoColor"/> of a pixel at the given position.
     /// </summary>
     /// <param name="position">Position of the pixel.</param>
@@ -53,20 +39,12 @@ public partial class Canvas : ScreenObject, IDisposable
     }
 
     /// <summary>
-    /// Retrieves the color of a pixel at the given position.
-    /// </summary>
-    /// <param name="position">Position of the pixel.</param>
-    /// <returns><see cref="Color"/> of the pixel.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when the position of the pixel is invalid.</exception>
-    public Color GetPixel(Point position) => GetMonoColor(position).ToColor();
-
-    /// <summary>
-    /// Retrieves the color of a pixel at the given position.
+    /// Retrieves the <see cref="MonoColor"/> of a pixel at the given position.
     /// </summary>
     /// <param name="position">Position of the pixel.</param>
     /// <returns><see cref="MonoColor"/> of the pixel.</returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown when the position of the pixel is invalid.</exception>
-    public MonoColor GetMonoColor(Point position)
+    public MonoColor GetPixel(Point position)
     {
         int index = position.ToIndex(Width);
         if (index < 0 || index >= Size) throw new ArgumentOutOfRangeException(OutOfRangeMsg);
